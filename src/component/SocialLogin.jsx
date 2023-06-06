431361dff33bbac5123a4199dbf1b1b9cd274609
Emon <emon.mhk69@gmail.com>
2023-06-07 00:27:02 +0600

    login page style done

---
+++ src/component/SocialLogin.jsx
@@ -0,0 +1,11 @@
+import { FaGoogle } from 'react-icons/fa';
+
+const SocialLogin = () => {
+    return (
+        <div className="w-full ">
+            <button className="p-4 btn-ghost text-orange-400 text-lg rounded-xl w-full flex justify-center"><FaGoogle></FaGoogle></button>
+        </div>
+    );
+};
+
+export default SocialLogin;
--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,8 +1,67 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+import {Link} from "react-router-dom";
+import { useForm } from "react-hook-form";
+import SocialLogin from "../../component/SocialLogin";
+import login from "../../assets/signin.jpg";
 
 const SignIn = () => {
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
-        <div>
-                login            
+        <div className='mt-5' >
+            <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center gap-5 p-2">
+                <div className="card w-full max-w-sm drop-shadow-2xl bg-base-100 order-2 md:order-1">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <h2 className="text-3xl font-bold text-center text-violet-700">Login</h2>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name="email" {...register("email", { required: true })} className="p-2 border-2 rounded-lg w-full" />
+                            {errors.email && <span className="text-red-600 ">email is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password"  {...register("password", {
+                                required: true,
+                                minLength: 6,
+                                maxLength: 20,
+                                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                            })} placeholder="password" className="input input-bordered" />
+                            {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                            {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                            {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                            {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+                        </div>
+
+                        <input type="submit" value="Login" className="btn btn-info mt-2"  />
+
+                    </form>
+                    <div className="px-4 pb-2">
+                        <p className="text-center text-gray-600">
+                            Don't have an Account? <Link className="text-sky-600" to="/signup">Create an account</Link>
+                        </p>
+                        <div className="divider">OR Continue With</div>
+
+                        <SocialLogin></SocialLogin>
+                        
+                    </div>
+
+                </div>
+                <div className="max-w-lg order-1 md:order-2">
+                    <img className="w-full" src={login} alt="login image" />
+                </div>
+            </div>
+
+
         </div>
     );
 };
